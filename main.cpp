8e112e11b3c69b339bf93ac30f25a230787d5552
lycanfloof <lyca2206@gmail.com>
2023-06-18 23:52:03 -0500

    Done some experiments with C++ stuff.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,46 @@
+#include <memory>
+
+void* operator new(size_t size)
+{
+    return malloc(size);
+}
+
+void operator delete(void* memory)
+{
+    free(memory);
+}
+
+class DynamicSet
+{
+    public:
+        DynamicSet();
+        virtual int Size() = 0;
+};
+
+DynamicSet::DynamicSet() {}
+
+class Example: public DynamicSet
+{
+    private:
+        int size;
+    public:
+        Example();
+        int Size() override;
+        ~Example();
+};
+
+Example::Example(): size(0) {}
+
+int Example::Size()
+{
+    return size;
+}
+
+Example::~Example() {}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    Example* e = new Example();
+    delete e;
     return 0;
 }
